6def390d863723148fc0bae7397799e1fd09b386
jxaqi <emilyfoo1998@gmail.com>
2021-10-24 18:14:23 +0800

    modify navbar and add a banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sig-market-revamp';
+
+  ngOnInit() {
+    $(document).ready(function() {
+      $("#input-search").keyup(function() {
+        if ($("#input-search").val().length > 0) {
+          $(".go-icon").addClass("go-in");
+        } else {
+          $(".go-icon").removeClass("go-in");
+        }
+      });
+      $(".go-icon").click(function() {
+        $(".search-form").submit();
+      });
+    });
+  }
 }
