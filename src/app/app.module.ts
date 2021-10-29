2e4db30e4582b26a58c5f798e838fb0e407b7076
jxaqi <emilyfoo1998@gmail.com>
2021-10-29 18:22:11 +0800

    generate three new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MonthlyExclusiveComponent } from './monthly-exclusive/monthly-exclusive.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
+import { CommunityComponent } from './community/community.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
-    MonthlyExclusiveComponent
+    MonthlyExclusiveComponent,
+    BestSellerComponent,
+    CommunityComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/app/monthly-exclusive/monthly-exclusive.component.ts
+++ src/app/monthly-exclusive/monthly-exclusive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-monthly-exclusive',
   templateUrl: './monthly-exclusive.component.html',
@@ -10,6 +12,8 @@ export class MonthlyExclusiveComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // $(document).ready(function () {
+    //   $("[data-bs-toggle='tooltip']").tooltip('show');
+    // });
   }
-
 }
